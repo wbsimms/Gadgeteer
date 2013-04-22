b21d392c6ed82e24ae07ee6f7d3e0f142c02dff7
Barrett Simms <wbsimms@gmail.com>
2013-04-21 20:13:17 -0400

    Move ellipse around the screem

--- Joystick_2_1/Joystick_2_1/Program.cs
+++ Joystick_2_1/Joystick_2_1/Program.cs
@@ -22,6 +22,10 @@ namespace Joystick_2_1
         private Font f = null;
         private int fontHeight = 0;
         private int lastHeight = 1;
+        private Joystick.Position currentPosition = new Joystick.Position();
+        private uint displayHeight, displayWidth = 0;
+
+
         // This method is run when the mainboard is powered up or reset.   
         void ProgramStarted()
         {
@@ -41,9 +45,16 @@ namespace Joystick_2_1
 
             // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
             Debug.Print("Program Started");
+            displayHeight = display_T35.Height;
+            displayWidth = display_T35.Width;
+            currentPosition.X = displayWidth/2.0;
+            currentPosition.Y = displayHeight/2.0;
+            MoveCursor();
+
             f = Resources.GetFont(Resources.FontResources.NinaB);
             fontHeight = f.Height;
             joystickPosition = joystick.GetJoystickPosition();
+
             joystickThread = new Thread(JoystickReadThread);
             joystickThread.Start();
             button.ButtonPressed += button_ButtonPressed;
@@ -59,19 +70,39 @@ namespace Joystick_2_1
             while (true)
             {
                 if (runJoystickThread == false) return;
-                Joystick.Position newJoystickPosition = joystick.GetJoystickPosition();
-                double newX = System.Math.Abs(joystickPosition.X - newJoystickPosition.X);
-                double newY = System.Math.Abs(joystickPosition.Y - newJoystickPosition.Y);
-                if (newX >= 0.02 || newY >= 0.02)
-                    DisplayText(joystickPosition.X + " " + joystickPosition.Y);
-                joystickPosition = newJoystickPosition;
+                MoveCursor();
                 Thread.Sleep(100);
             }
         }
 
+        public void MoveCursor()
+        {
+            double realX = 0, realY = 0;
+            Joystick.Position newJoystickPosition = joystick.GetJoystickPosition();
+            double newX = joystickPosition.X-.5+currentPosition.X - currentPosition.X;
+            double newY = joystickPosition.Y-.5+currentPosition.Y - currentPosition.Y;
+            joystickPosition = newJoystickPosition;
+
+            // did we actually move...
+            if (System.Math.Abs(newX) >= 0.03){realX = newX;}
+            if (System.Math.Abs(newY) >= 0.03){realY = newY;}
+            if (realX == 0.0 && realY == 0.0) return;
+
+            if (realX + currentPosition.X >= displayWidth) currentPosition.X = 0;
+            if (realX + currentPosition.X <= 0) currentPosition.X = displayWidth;
+            if (realY + currentPosition.Y >= displayHeight) currentPosition.Y = 0;
+            if (realY + currentPosition.Y <= 0) currentPosition.Y = displayHeight;
+
+            Debug.Print(realX + " " + realY);
+
+            currentPosition.X += realX*5;
+            currentPosition.Y += realY*5;
+            display_T35.SimpleGraphics.DisplayEllipse(Color.White, (uint)currentPosition.X, (uint)currentPosition.Y, 2, 2);
+        }
+
         public void DisplayText(string text)
         {
-            if (lastHeight >= display_T35.Height)
+            if (lastHeight >= displayHeight)
             {
                 display_T35.SimpleGraphics.Clear();
                 lastHeight = 1;
