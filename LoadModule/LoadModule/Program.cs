5b92b891c181893b96ad94aa449e91c0c8452b69
Barrett Simms <wbsimms@gmail.com>
2013-05-05 12:28:55 -0400

    Load Example

---
+++ LoadModule/LoadModule/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+
+namespace LoadModule
+{
+    public partial class Program
+    {
+        public static bool runMotor = false;
+        
+        void ProgramStarted()
+        {
+            Debug.Print("Program Started");
+            button.ButtonPressed += button_ButtonPressed;
+        }
+
+        void button_ButtonPressed(GTM.GHIElectronics.Button sender, GTM.GHIElectronics.Button.ButtonState state)
+        {
+            if (!runMotor)
+            {
+                // run motor
+                runMotor = true;
+                load.P1.Write(true);
+            }
+            else
+            {
+                runMotor = false;
+                load.P1.Write(false);
+            }
+
+
+        }
+    }
+}
