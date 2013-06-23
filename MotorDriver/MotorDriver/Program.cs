b962516d85afb267b97d74a2b4e0d05dd0bf690f
Barrett Simms <wbsimms@gmail.com>
2013-06-23 11:16:35 -0400

    update

--- MotorDriver/MotorDriver/Program.cs
+++ MotorDriver/MotorDriver/Program.cs
@@ -16,25 +16,9 @@ namespace MotorDriver
 {
     public partial class Program
     {
-        // This method is run when the mainboard is powered up or reset.   
         private bool touch = false;
         void ProgramStarted()
         {
-            /*******************************************************************************************
-            Modules added in the Program.gadgeteer designer view are used by typing 
-            their name followed by a period, e.g.  button.  or  camera.
-            
-            Many modules generate useful events. Type +=<tab><tab> to add a handler to an event, e.g.:
-                button.ButtonPressed +=<tab><tab>
-            
-            If you want to do something periodically, use a GT.Timer and handle its Tick event, e.g.:
-                GT.Timer timer = new GT.Timer(1000); // every second (1000ms)
-                timer.Tick +=<tab><tab>
-                timer.Start();
-            *******************************************************************************************/
-
-
-            // Use Debug.Print to show messages in Visual Studio's "Output" window during debugging.
             Debug.Print("Program Started");
 
             motorControllerL298.DebugPrintEnabled = true;
@@ -43,9 +27,6 @@ namespace MotorDriver
             Panel panel = new Panel();
             window.Child = panel;
             panel.TouchDown += panel_TouchDown;
-
-              
-
         }
 
         void panel_TouchDown(object sender, Microsoft.SPOT.Input.TouchEventArgs e)
