f14bc1922b4ba57a664c7d49115c0b9f05b359d5
Barrett Simms <wbsimms@gmail.com>
2013-06-29 10:14:57 -0400

    Initial Setup

---
+++ DistanceLoggerWithWebReporting/DistanceLoggerWithWebReporting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+
+namespace DistanceLoggerWithWebReporting
+{
+    public partial class Program
+    {
+        void ProgramStarted()
+        {
+        }
+    }
+}
